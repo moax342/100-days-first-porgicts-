909fcd041b6a408bda2bed08f9ebb782707b5af6
Mohammed Altoum <63297751+moax342@users.noreply.github.com>
2024-03-09 14:18:47 +0200

    Update password genrator.py

--- password genrator.py
+++ password genrator.py
@@ -1,4 +1,4 @@
-import random
+from random import choice,shuffle
 letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'
          ,'p','q','r','s','t','u','v','w','x','y','z']
 numbers=['1','2','3','4','5','6','7','8','9']
@@ -8,12 +8,10 @@ n_letters=int(input("how many letter do you want "))
 n_numbers=int(input("how many number do you want"))
 n_symbles=int(input("how many symbel do you want"))
 
-password=''
-for let in range(0,n_letters):
-    password+=random.choice(letters)
-    if let<n_numbers:
-        password+=random.choice(numbers)
-    if let <n_symbles:
-        password+=random.choice(symbles)
-    
-print(f'your password is : {password}')
+password=[]
+password=[choice(letters) for l in range(n_letters)]
+password += [choice(symbols) for char in range(n_numbers)]
+password += [choice(numbers) for n in range(n_symbles)]    
+shuffle(password)
+final_password="".join(password)
+print(f'your password is : {final_password}')
